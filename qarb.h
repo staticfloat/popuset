6f8f6e1758fbb1ddd40cb56bcea527bcb44c5148
Elliot Saba <staticfloat@gmail.com>
2017-04-04 22:23:10 -0700

    Add a bit of documentation for the qarb

--- qarb.h
+++ qarb.h
@@ -1,6 +1,18 @@
 #include <map>
 #include <string>
 
+/*
+The Queueing Additive Ring Buffer (QARB, pronounced "Carb") is a datastructure
+used to simplify the mixing together of multiple client audio streams into a
+single resultant audio stream in realtime.  This is done by having a single
+underlying ring buffer that is added into by multiple clients, all of which
+track their own respective location within the ring buffer.
+
+The underlying ring buffer is read from at a particular rate; clients that
+overrun this rate and the amount of available buffer space have their excess
+output discarded, clients that underrun this rate create discontinuities in
+their output.
+*/
 class QueueingAdditiveRingBuffer {
 public:
 	QueueingAdditiveRingBuffer( const unsigned int len );
